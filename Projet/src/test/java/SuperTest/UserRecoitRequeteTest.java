a5ec76d91fa7fba837ff7814e652e1445d9691ff
RomainMln <moulin.romain@hotmail.com>
2022-12-02 14:42:35 +0100

    correction test (il passe toujours pas, faut tester conversation avant)

--- Projet/src/test/java/SuperTest/UserRecoitRequeteTest.java
+++ Projet/src/test/java/SuperTest/UserRecoitRequeteTest.java
@@ -1,6 +1,7 @@
 package SuperTest;
 
 import Conversation.ConversationManager;
+import NetworkManager.ThreadTcpReceiveConnection;
 import UserList.ListeUser;
 import UserList.UserAlreadyInListException;
 import org.junit.Test;
@@ -9,12 +10,16 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import static java.lang.Thread.sleep;
+
 public class UserRecoitRequeteTest {
 
     @Test
-    public void recoitRequeteTest() throws UnknownHostException {
-        InetAddress addressUserLanceRequete = InetAddress.getByName(""); // TODO
+    public void recoitRequeteTest() throws UnknownHostException, InterruptedException {
+        InetAddress addressUserLanceRequete = InetAddress.getByName("insa-20670.insa-toulouse.fr"); // TODO
+        ThreadTcpReceiveConnection threadRcv = new ThreadTcpReceiveConnection();
         ListeUser.addUser(3456, "aude", addressUserLanceRequete);
+        sleep(30000);
         try {
             ConversationManager.getConv(3456).sendMessage("coucou user1");
         } catch (IOException e) {
