eb90a066d18e1d217c4139f1a4d14f8ba657adc6
Aude Jean-Baptiste <jeaaud1@alum.us.es>
2022-12-14 10:17:11 +0100

    ajout classe main

---
+++ Projet/src/main/java/Main.java
@@ -0,0 +1,17 @@
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+
+
+public class Main {
+
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args){
+        Configurator.setRootLevel(Level.INFO); // only show INFO message in the application (debug are ignored)
+        LOGGER.info("démarrage de l'application");
+
+    }
+
+}
