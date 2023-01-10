06b5b0b6256b007af6c385ad5945764b6b4a89da
RomainMln <moulin.romain@hotmail.com>
2023-01-10 17:17:59 +0100

    DatabaseManager Terminé (pas encore testé)

--- Projet/src/main/java/chavardage/database/DatabaseManager.java
+++ Projet/src/main/java/chavardage/database/DatabaseManager.java
@@ -1,6 +1,8 @@
 package chavardage.database;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class DatabaseManager {
 
@@ -24,9 +26,10 @@ public class DatabaseManager {
     public static DatabaseManager getInstance() {return instance;}
 
     public synchronized ResultSet getMessages(int idUserLocal, int idUserDistant) throws SQLException{
+        Statement retriveMessage = database.createStatement();
         int minId = Math.min(idUserLocal,idUserDistant);
         int maxId = Math.max(idUserLocal,idUserDistant);
-        ResultSet result = statement.executeQuery("SELECT date,sentBy,message FROM conversation AS c JOIN message as m ON c.userId1=" + minId  + "AND c.userId2=" + maxId + " AND c.idConversation=m.convId;");
+        ResultSet result = retriveMessage.executeQuery("SELECT date,sentBy,message FROM conversation AS c JOIN message as m ON c.userId1=" + minId  + "AND c.userId2=" + maxId + " AND c.idConversation=m.convId;");
         return result;
     }
 
@@ -36,16 +39,27 @@ public class DatabaseManager {
     }
 
     //TODO Insérer message dans la table
-    public synchronized  void insertMessage(int idUserLocal, int idUserDistant, String message) throws SQLException{
+    public synchronized  void insertMessage(int idUserLocal, int idUserDistant, String message, LocalDateTime dateTime, int sentBy) throws SQLException{
+
         int minId= Math.min(idUserLocal,idUserDistant);
         int maxId= Math.max(idUserLocal,idUserDistant);
+        int convId;
+        ResultSet convAlreadyExist = statement.executeQuery("SELECT * FROM conversation WHERE userId1=" + minId + " AND userId2=" + maxId + ";");
+        ResultSet  convIdQuerry;
 
-        ResultSet result = statement.executeQuery("SELECT * FROM conversation WHERE userId1=" + minId + " AND userId2=" + maxId + ";");
 
-        if(!result.next()){
+        if(!convAlreadyExist.next()){
                 statement.executeQuery("INSERT INTO conversation  (userId1,userId2) VALUES (" + minId + "," + maxId +");");
         }
 
+        convAlreadyExist.close();
 
+        convIdQuerry = statement.executeQuery("SELECT conversationId FROM conversation as c WHERE c.userId1=" + minId + " AND userId2=" + maxId + ";");
+        convIdQuerry.next();
+        convId = convIdQuerry.getInt("idconversation");
+        convIdQuerry.close();
+
+        statement.executeQuery("INSERT INTO message (convId,date,sentBy,Message) VALUES " +
+                "(" + convId + "," + dateTime + "," + sentBy + "," + message + ");");
     }
 }
