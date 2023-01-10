25ee19a384d1c650f93d6feadec446f6f2da4315
Aude510 <dunae97300@outlook.fr>
2023-01-10 14:50:50 +0100

    gestion d'exceptions

--- Projet/src/main/java/chavardage/Main.java
+++ Projet/src/main/java/chavardage/Main.java
@@ -3,14 +3,21 @@ package chavardage;
 import chavardage.chavardageManager.AlreadyUsedPseudoException;
 import chavardage.chavardageManager.ChavardageManager;
 import chavardage.chavardageManager.GestionUDPMessage;
+import chavardage.chavardageManager.UsurpateurException;
+import chavardage.conversation.ConversationAlreadyExists;
+import chavardage.conversation.ConversationDoesNotExist;
 import chavardage.conversation.ConversationManager;
+import chavardage.message.TCPMessage;
 import chavardage.networkManager.TCPServeur;
 import chavardage.networkManager.UDPServeur;
 import chavardage.userList.ListeUser;
+import chavardage.userList.UserNotFoundException;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
+
+import java.io.IOException;
 
 
 public class Main {
@@ -32,14 +39,46 @@ public class Main {
         chavardageManager.notifyChangePseudo(listeUser.getMySelf());
     }
 
+    private static void clear(){
+        listeUser.clear();
+        conversationManager.clear();
+    }
 
 
     public static void main(String[] args){
+        clear();
         Configurator.setRootLevel(Level.INFO); // only show INFO message in the application (debug are ignored)
         LOGGER.info("démarrage de l'application");
 
         UDPServeur udpServeur = new UDPServeur(gestionUDPMessage);
         TCPServeur tcpServeur = new TCPServeur(conversationManager);
+
+        listeUser.setMyself(1,"Aude");
+
+        try {
+            chavardageManager.connectToApp(listeUser.getMySelf());
+        } catch (InterruptedException | UsurpateurException | AlreadyUsedPseudoException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            conversationManager.openConversation(2);
+        } catch (UserNotFoundException | AssignationProblemException | IOException | ConversationAlreadyExists e) {
+            e.printStackTrace();
+        }
+
+        try {
+            conversationManager.getSendData(2).envoyer(new TCPMessage(2,1,"salut bg"));
+        } catch (ConversationDoesNotExist conversationDoesNotExist) {
+            conversationDoesNotExist.printStackTrace();
+        }
+
+        try {
+            conversationManager.fermerConversation(2);
+        } catch (ConversationDoesNotExist conversationDoesNotExist) {
+            conversationDoesNotExist.printStackTrace();
+        }
+
     }
 
 }
