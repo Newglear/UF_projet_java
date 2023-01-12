b68c2c6b2fb1da267bafcb41283bc30370d30af0
RomainMln <moulin.romain@hotmail.com>
2023-01-12 12:16:44 +0100

    hover des user connected

--- javaFXtest/src/main/java/com/example/javafxtest/User.java
+++ javaFXtest/src/main/java/com/example/javafxtest/User.java
@@ -1,7 +1,11 @@
 package com.example.javafxtest;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class User {
 
     @FXML
@@ -10,10 +14,23 @@ public class User {
     private Label id;
     @FXML
     private Label lastMessage;
+    @FXML
+    private AnchorPane background;
 
     public Label getUsername(){return username;}
 
     public Label getId(){return id;}
 
-    //public Label getLastMessage(){return lastMessage;}
+    public void onClick(ActionEvent event){
+
+    }
+
+    public void hover(MouseEvent event){
+        background.setStyle("-fx-background-color:#555555");
+    }
+
+    public void mouseExited(MouseEvent event){
+        background.setStyle("-fx-background-color:#333333");
+    }
+    public Label getLastMessage(){return lastMessage;}
 }
