130072350d01edc488789c7b407569dbbb0abe7d
Aude510 <dunae97300@outlook.fr>
2023-01-16 14:45:12 +0100

    début ménage network manager

--- Projet/src/main/java/chavardage/networkManager/TCPConnect.java
+++ Projet/src/main/java/chavardage/networkManager/TCPConnect.java
@@ -1,13 +1,10 @@
 package chavardage.networkManager;
 
 import chavardage.conversation.NetworkException;
-import chavardage.message.TCPMessage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
@@ -16,19 +13,7 @@ public class TCPConnect {
     private static final Logger LOGGER = LogManager.getLogger(TCPConnect.class);
 
 
-    /** se connecte sur le port TCP par défaut*/
-    public static Socket connectTo(InetAddress address) throws NetworkException {
-        try {
-            Socket socket = new Socket(address, TCPServeur.DEFAULT_PORT_TCP);
-            LOGGER.trace("Connection réalisée avec " + address + " sur le socket " + socket);
-            return socket;
-        } catch (IOException e) {
-            LOGGER.error(e.getMessage());
-            throw new NetworkException("TCPConnect");
-        }
-    }
-
-    /** se connecte sur le port donné*/
+    /** se connecte sur le port donné, retourne un socket et throw une NetworkException si quelque chose se passe mal*/
     public static Socket connectTo(InetAddress address, int port) throws NetworkException {
         try {
             Socket socket = new Socket(address, port);
